258e1c633a3224818a3cea6f96407942a67a33ee
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-12 17:39:35 +0530

    conditonal statement

--- first.php
+++ first.php
@@ -10,6 +10,30 @@
     <h1>My first PHP web page</h1>
     <?php
     echo "Hello world";
+    echo "<br>";
+    echo "<br>";
+
+                         //  This is a conditional statement & switch case
+                        //  This is a conditional statement & switch case
+
+    $level = 4;
+    switch($level) {
+        case 1:
+            echo "You are playing at EASY level";
+            break;
+        case 2;
+            echo "You are playing at MODERATE level";
+            break;
+        case 3;
+             echo "You are playing at HARD level";
+             break;
+        case 4;
+            echo "You are playing at EXTREME level";
+            break;
+        default:
+             echo "Invalid Input ";
+    }
+
     ?>
 </body>
 </html>

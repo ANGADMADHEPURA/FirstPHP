6dd2a64a2ba743a326e76b275a002f55290ec404
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-13 10:12:35 +0530

    this is a class

--- class.php
+++ class.php
@@ -13,10 +13,10 @@
         public $name;
         public $color;
         function set_name ($name) {
-            $this => name = $name;
+            $this -> name = $name;
         }
         function get_name () {
-            return $this => name;
+            return $this -> name;
         }
     }
     ?>

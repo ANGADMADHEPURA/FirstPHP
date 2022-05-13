18e1e55126f62949559f87fab994ce989084c374
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-13 08:53:59 +0530

    add javaScript file

---
+++ script.js
@@ -0,0 +1,40 @@
+// let num = 65 - 5;
+// console.log(num)
+
+// num = 20 + 2;
+// console.log(num)
+// console.log(2+6+8+4)
+
+// let username = "angad mandal"
+// console.log(username)
+
+// var array = [1,2,3]
+
+// for(var i = 0; i < array.length ; i++){
+//     console.log(array[i])
+// }
+
+// function findLion(animal) {
+//     return animal === "lion";
+//  }
+//  let fillEle = document.querySelector(".find");
+//  let arr = ["cow", "lion", "bull", "tiger"];
+//  fillEle.innerHTML = arr;
+//  document.querySelector(".fillArr").addEventListener("click", () => {
+//     fillEle.innerHTML = arr.find(findLion);
+//  });
+
+var principle = document.getElementById("principle");
+var roi = document.getElementById("roi");
+var years = document.getElementById("years");
+var outputspan = document.getElementById("outputspan");
+
+//itvoyagers.in
+function countSI()
+{
+	p = parseFloat(principle.value);
+	n = parseFloat(years.value);
+	r = parseFloat(roi.value);
+	si = (p*n*r)/100;
+	outputspan.innerHTML = si;
+}

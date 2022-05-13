294ff74df9c9c962ad0aa1769dc7122415fec114
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-13 10:10:05 +0530

    class

--- class.php
+++ class.php
@@ -7,6 +7,17 @@
     <title>i am learning php class</title>
 </head>
 <body>
-    
+    <?php
+    class  fruit {
+        public $name;
+        public $color;
+        function set_name ($name) {
+            $this => name = $name;
+        }
+        function get_name () {
+            return $this => name;
+        }
+    }
+    ?>
 </body>
 </html>

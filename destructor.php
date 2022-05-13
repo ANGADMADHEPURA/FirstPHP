cf344d09e11e3cc6f9f1d302ea2a2c72bc7b2c8f
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-13 16:46:52 +0530

    destructor in php working fine

---
+++ destructor.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example of destructor in php</title>
+</head>
+<body>
+    <?php
+class Fruit {
+    // Properties
+    var $name;
+    var $color;
+  
+    // Methods
+    function __construct($name, $color) {
+      $this->name = $name;
+      $this->color = $color; 
+    }
+    function __destruct() {
+      echo "The fruit is {$this->name} and the color is {$this->color}."; 
+    }
+  }
+  
+  $apple = new Fruit("Apple", "red");
+          ?>
+          <h1>This is a destructor example of PHP</h1>
+
+</body>
+</html>

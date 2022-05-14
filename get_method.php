b9763bc023d453292a8316f5214d2973f1003452
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-15 00:04:10 +0530

    php in get_method using enter name and age

---
+++ get_method.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Example of get method in php </title>
+</head>
+<body>
+    <form action="<?php $_PHP_SELF ?>" method="GET">
+    Name: <input type="text" name="name" />
+    Age: <input type="text" name="age" />
+    <input type="submit" />
+        </form>
+    <?php
+    if(isset($_GET["name"])  || isset($_GET["age"]))  {
+        echo "HEllO" . $_GET ['name']. "<br/>";
+        echo "Your age is " . $_GET['age']. "years";
+        exit(); 
+    }
+    ?>
+</body>
+</html>

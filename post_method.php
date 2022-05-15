e0bb2e88a70b2591e5bcfb59bf0fa78d52df8c86
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-15 11:09:37 +0530

    working properly post method

--- post_method.php
+++ post_method.php
@@ -7,6 +7,21 @@
     <title>Example of post method in php</title>
 </head>
 <body>
-    
+    <?php
+    if(isset($_POST["name"]) || isset($_POST["age"])) {
+        if(preg_match("Aa-Zz", $_POST['name'])) {
+            die("Invalid name . name should be alphabet");
+        }
+        echo "Hello". $_POST['name'] . "<br/>"; 
+        echo "Your age is". $_POST['age']. "<br/>";
+        exit();
+    }
+    ?>
+    <form action="<?php $_PHP_SELF ?>" method="POST">
+    <center>
+         Name: <input type="text" name="name"  /> <br>  
+         Age: <input type="text" name="age" /> <br>
+       <input type="submit"  /> </center> 
+         </form>
 </body>
 </html>

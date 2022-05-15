eb4dd6aa86aa936db97292d60960d42b65356dde
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-15 17:37:27 +0530

    almost done

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
  if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-     $conn= mysqli_connect('localhost', 'root', 'test') or die("Connection Failed:" .mysqli_connect_error());
+     $conn= mysqli_connect('localhost', 'root', 'user') or die("Connection Failed:" .mysqli_connect_error());
      if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
          $name= $_POST['name'];
          $email= $_POST['email'];
@@ -9,7 +9,7 @@
 
          $sql= "INSERT INTO 'users' ('name', 'email', 'phone', 'bgroup') VALUES ('$name', '$email', '$phone', '$bgroup') ";
 
-         $query = mysql_query($conn, $sql);
+         $query = mysqli_query($conn, $sql);
          if($query) {
              echo 'Entry successfull';
          }

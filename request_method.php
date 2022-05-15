9fb62b4ea12c408661e46c66eee9762432ab23b6
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-15 11:23:56 +0530

    working request method

--- request_method.php
+++ request_method.php
@@ -7,6 +7,17 @@
     <title>Example of request method in php (Hypertext pre-processor)</title>
 </head>
 <body>
-    
+    <?php
+    if(isset($_REQUEST["name"]) || isset($_POST["age"])) {
+        echo "Hi". $_REQUEST['name']. "<br/>";
+        echo "Your age is ". $_REQUEST['age']. "years";
+        exit();
+    }
+    ?>
+    <form action="<?php $_PHP_SELF ?>" method="POST">
+       Name: <input type="text" name="name" />
+       Age: <input type="text" name="age" />
+       <input type="submit" />
+       </form>
 </body>
 </html>

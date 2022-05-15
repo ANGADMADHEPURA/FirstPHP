e6bd6e6266eec029561e7d3aad08c856d0b7227f
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-15 17:57:43 +0530

    not entry

--- connect.php
+++ connect.php
@@ -1,13 +1,13 @@
 <?php
  if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-     $conn= mysqli_connect('localhost', 'root', 'user') or die("Connection Failed:" .mysqli_connect_error());
+     $conn= mysqli_connect('localhost', 'root', '', 'firstconnectdatabasetophp') or die("Connection Failed:" .mysqli_connect_error());
      if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
          $name= $_POST['name'];
          $email= $_POST['email'];
          $phone= $_POST['phone'];
          $bgroup= $_POST['bgroup'];
 
-         $sql= "INSERT INTO 'users' ('name', 'email', 'phone', 'bgroup') VALUES ('$name', '$email', '$phone', '$bgroup') ";
+         $sql= "INSERT INTO 'firstconnectdatabasetophp' ('name', 'email', 'phone', 'bgroup') VALUES ('$name', '$email', '$phone', '$bgroup') ";
 
          $query = mysqli_query($conn, $sql);
          if($query) {

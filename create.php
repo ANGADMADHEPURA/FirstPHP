e5256c587d03cf5d4bb7852da2c980bc2445d672
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-16 15:14:02 +0530

    insert into

--- create.php
+++ create.php
@@ -9,4 +9,6 @@
              $gender = $_POST['gender'];
          }
 
-         $sql = 
+         $sql = "INSERT INTO 'users' ('firstname', 'lastname', 'email', 'password', 'gender') VALUE  ('$first_name', '$last_name', '$email', '$password', '$gender')";
+
+         

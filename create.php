bccd9aed26950360c7ae1f76fdabf46d53acd6c0
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-16 15:28:37 +0530

    this is a show to create php php code and html  code

--- create.php
+++ create.php
@@ -11,4 +11,48 @@
 
          $sql = "INSERT INTO 'users' ('firstname', 'lastname', 'email', 'password', 'gender') VALUE  ('$first_name', '$last_name', '$email', '$password', '$gender')";
 
-         
+         $result = $conn->query($sql);
+
+         if($result == TRUE) {
+             echo "New record created successfully";
+
+         }
+         else {
+             echo "Error:" . $sql . "<br>" . $conn->error;
+         }
+
+         $conn->close();
+
+        ?>
+
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>SignUp Form</title>
+        </head>
+        <body>
+            <h2>SignUp Form</h2>
+            <form action="" method="POST">
+                <fieldset>
+                    <legend>Personal Information</legend>
+                    First Name: <br>
+                    <input type="text" name="firstname">
+                    <br>
+                    Last Name: <br>
+                    <input type="text" name="lastname">
+                    <br>
+                    Password: <br>
+                    <input type="password" name="password">
+                    <br>
+                    Gender: <br>
+                    <input type="radio" name="gender" value="Male">Male
+                    <input type="radio" name="gender" value="Female">Female
+                    <br><br>
+                    <input type="submit" name="submit" value="submit">
+                </fieldset>
+            </form>
+        </body>
+        </html>

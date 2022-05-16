2baaed9b2511fff7b62015913f8abb0ffcfd134c
ANGADMADHEPURA <angadraj9534@gmail.com>
2022-05-17 00:25:06 +0530

    working

--- script.js
+++ script.js
@@ -24,17 +24,20 @@
 //     fillEle.innerHTML = arr.find(findLion);
 //  });
 
-var principle = document.getElementById("principle");
-var roi = document.getElementById("roi");
-var years = document.getElementById("years");
-var outputspan = document.getElementById("outputspan");
+// var principle = document.getElementById("principle");
+// var roi = document.getElementById("roi");
+// var years = document.getElementById("years");
+// var outputspan = document.getElementById("outputspan");
 
 //itvoyagers.in
-function countSI()
-{
-	p = parseFloat(principle.value);
-	n = parseFloat(years.value);
-	r = parseFloat(roi.value);
-	si = (p*n*r)/100;
-	outputspan.innerHTML = si;
-}
+// function countSI()
+// {
+// 	p = parseFloat(principle.value);
+// 	n = parseFloat(years.value);
+// 	r = parseFloat(roi.value);
+// 	si = (p*n*r)/100;
+// 	outputspan.innerHTML = si;
+// }
+   // JavaScript statement
+var a = 10+20;    // Another Statement
+document.write(a);
